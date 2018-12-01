1628619e6a986fb2a3b52ecb3c575632b0e4eaa4
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-12-01 18:36:11 +0700

    Added actions list

---
+++ src/app/search/actions/actions.js
@@ -0,0 +1,45 @@
+import fetch from 'cross-fetch'
+
+export const REQUEST_AUTH_INSTA = 'REQUEST_AUTH_INSTA'
+export const RESP_AUTH_INSTA_SUCCESS = 'RESP_AUTH_INSTA_SUCCESS'
+export const REQUEST_TAG_COUNT = 'REQUEST_TAG_COUNT'
+export const RECEIVE_TAG_COUNT_SUCCESS = 'RECEIVE_TAG_COUNT_SUCCESS'
+
+
+function requestAuthInsta(clientId) {
+    return {
+        type: REQUEST_AUTH_INSTA,
+        instaClientId : clientId
+    }
+}
+
+function respondAuthInstaSuccess(instaAccessToken) {
+    return {
+        type: RESP_AUTH_INSTA_SUCCESS,
+        instaAccessToken: instaAccessToken
+    }
+}
+
+function requestTagCount(tagName) {
+    return {
+        type: REQUEST_TAG_COUNT,
+        tagName: tagName
+    }
+}
+
+function receiveTagCountSuccess(jsonData) {
+    return {
+        type: RECEIVE_TAG_COUNT_SUCCESS,
+        tagName: jsonData.data.name,
+        tagCount : jsonData.data.media_count
+    }
+}
+
+function fetchTagCount(tagName, accessToken) {
+    return dispatch => {
+        dispatch(requestTagCount(tagName))
+        return fetch(`https://api.instagram.com/v1/tags/${tagName}?access_token=${accessToken}`)
+            .then(response => response.json())
+            .then(json => dispatch(receiveTagCountSuccess(json)))
+    }
+}
