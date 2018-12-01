898ffcd4f7bffc8fe4f5e3ed5e4b5699aea23024
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-12-01 20:32:23 +0700

    Added Search and TagsMap components to App

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import {SearchForm} from "./search/components/SearchForm";
+import {TagsCountList} from "./search/components/TagsCountList";
 
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <SearchForm />
+                <TagsCountList />
             </div>
         );
     }
