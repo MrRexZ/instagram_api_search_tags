7d0c13a79b2f133fc328ca9469cd16e8865cd529
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-12-01 23:19:10 +0700

    Implemented listing of tag count by tag

--- src/app/search/components/TagsCountList.js
+++ src/app/search/components/TagsCountList.js
@@ -2,10 +2,15 @@ import React, {Component} from 'react';
 
 export class TagsCountList extends Component {
     render() {
-        const tagsCountMap = this.props.tagsCountMap
+        const tagsCountMap = this.props.tagsCountMap.tagData
+        var tagsArr = Object.values(tagsCountMap)
+        console.log(tagsArr)
         return (
             <div>
                 <ul>
+                    {tagsArr.map(tagObj => (
+                        <li>{tagObj["tagName"]} - {tagObj["tagCount"]}</li>
+                    ))}
                 </ul>
             </div>
         )
