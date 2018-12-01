cd74a7f7e8cc6e9574071205db93ce2feac8fb5c
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-12-02 01:00:48 +0700

    Moved configureStore file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
-import configureStore from "./app/config/configureStore";
+import configureStore from "./config/configureStore";
 import {Provider} from "react-redux";
 
 
